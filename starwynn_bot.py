30f37cb768bd34e438da6ca6d61d6339d7e9f2d1
cyf_desktop <cyf@mit.edu>
2024-11-10 05:15:57 -0500

    bot ready up

--- starwynn_bot.py
+++ starwynn_bot.py
@@ -1,3 +1,26 @@
 import discord
-import starwynn_tracker
+import starwynn_tracker as tracker
 
+verbose = True
+tracker.verbose = False
+
+file = open('bot_token.txt','r')
+BOT_TOKEN = file.read()
+file.close()
+
+print(BOT_TOKEN)
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    if verbose:
+        print(f'{client.user} has connected to discord')
+
+client.run(BOT_TOKEN)
+
+
+if __name__ == "__main__":
+    esi_tracker = tracker.Tracker("sky", "ESI")
--- starwynn_tracker.py
+++ starwynn_tracker.py
@@ -2,13 +2,13 @@ import starwynn_utils as utils
 import time
 
 verbose = False
-
-utils.verbose = verbose
+utils.verbose = False
 
 class Tracker:
-    def __init__(self, claim):
+    def __init__(self, claim, guild):
         self.territories = utils.define_claim_from_csv(claim)
         self.current_territories = utils.find_claim_owners(self.territories)
+        self.guild = guild
         if verbose:
             print("tracker started on", claim)
     
