abe4bf718e52d1f67e0d681bc700083125c29048
cyf_desktop <cyf@mit.edu>
2024-11-10 04:55:34 -0500

    utils and tracker done

---
+++ starwynn_tracker.py
@@ -0,0 +1,34 @@
+import starwynn_utils as utils
+import time
+
+verbose = False
+
+utils.verbose = verbose
+
+class Tracker:
+    def __init__(self, claim):
+        self.territories = utils.define_claim_from_csv(claim)
+        self.current_territories = utils.find_claim_owners(self.territories)
+        if verbose:
+            print("tracker started on", claim)
+    
+    def update_territories(self):
+        if verbose:
+            print("updating territories")
+        new_territories = utils.find_claim_owners(self.territories)
+        updated_teritories = []
+
+        for terr in self.territories:
+            if new_territories[terr] != self.current_territories[terr]:
+                print(terr, "has been captured by", new_territories[terr], "from", self.current_territories[terr]) # this would be 
+                updated_teritories.append((terr, new_territories[terr], self.current_territories[terr]))
+
+        self.current_territories = new_territories
+        return updated_teritories
+
+
+if __name__ == "__main__":
+    tracker = Tracker("desert")
+    while True:
+        time.sleep(2)
+        print(tracker.update_territories())
---
+++ starwynn_utils.py
@@ -0,0 +1,64 @@
+import requests, csv
+
+verbose = True
+
+def get_terr(n = 10):
+    if verbose:
+        print("requesting territory information...")
+    request = requests.get("https://api.wynncraft.com/v3/guild/list/territory")
+    try:
+        assert request.status_code == 200
+        if verbose:
+            print("request successful!")
+        return request.json()
+    except AssertionError:
+        if verbose:
+            print("unsuccessful request")
+        return None
+
+
+def list_to_csv(data, file):
+    with open(file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        for item in data:
+            writer.writerow([item])
+
+def define_claim_from_guild(prefix, file):
+    try:
+        assert file[-4:] == ".csv"
+    except AssertionError:
+        file += ".csv"
+    territories = get_terr()
+
+    claim_list = []
+
+    for terr, info in territories.items():
+        if info["guild"]["prefix"] == prefix:
+            claim_list.append(terr)
+    list_to_csv(claim_list, file)
+
+    return claim_list
+
+def define_claim_from_csv(file):
+    try:
+        assert file[-4:] == ".csv"
+    except AssertionError:
+        file += ".csv"
+    with open(file, mode='r') as file:
+        reader = csv.reader(file)
+
+        territories = [row[0] for row in reader]
+    
+    return territories
+
+def find_claim_owners(territories):
+    live = get_terr()
+    return {terr: live[terr]["guild"]["prefix"] for terr in territories}
+
+if __name__ == "__main__":
+    sky = define_claim_from_csv("sky")
+    print(find_claim_owners(sky))
+
+
+# define_claim_from_guild("RATC", "sky")
